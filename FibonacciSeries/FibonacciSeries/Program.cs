d8a850ec23d68fd3b7b4f6c8142010e6194b10c2
Abel Lopez <abelopezpaniagua@gmail.com>
2021-06-17 16:20:52 -0400

    fibonacci series application implemented

---
+++ FibonacciSeries/FibonacciSeries/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FibonacciSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = Fibonacci(6);
+            Console.WriteLine(result);
+            Console.ReadKey(true);
+        }
+
+        static string Fibonacci(int n)
+        {
+            int n1 = 0;
+            int n2 = 1;
+            int sum = 0;
+
+            string fibonacciSeries = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                fibonacciSeries += $" {n1}";
+
+                sum = n1 + n2;
+                n1 = n2;
+                n2 = sum;
+            }
+
+            return fibonacciSeries;
+        }
+    }
+}
+//0 1 1 2 3 5
