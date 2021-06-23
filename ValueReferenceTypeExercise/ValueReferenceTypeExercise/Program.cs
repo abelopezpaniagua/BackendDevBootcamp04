569e1ddf5b0fa56b4d8868dee7576eb0ffd07425
Abel Lopez <abelopezpaniagua@gmail.com>
2021-06-23 17:15:29 -0400

    ValueReferenceTypeExercise Program

---
+++ ValueReferenceTypeExercise/ValueReferenceTypeExercise/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace ValueReferenceTypeExercise
+{
+    class Program
+    {
+        public class DetailedInteger : ICloneable
+        {
+            public int Number { get; set; }
+            public List<string> Details { get; set; }
+
+            public DetailedInteger(int number)
+            {
+                Number = number;
+                Details = new List<string>();
+            }
+
+            public DetailedInteger(DetailedInteger detailedInteger)
+            {
+                this.Number = detailedInteger.Number;
+                this.Details = detailedInteger.Details;
+            }
+
+            public void AddDetail(string detail)
+            {
+                Details.Add(detail);
+            }
+
+            public override string ToString()
+            {
+                return $"{Number} [{ string.Join(',', Details) }]";
+            }
+
+            public object Clone()
+            {
+                return this.MemberwiseClone();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var n1 = new DetailedInteger(0);
+            n1.AddDetail("A");
+            Console.WriteLine(n1);
+
+            var n2 = n1;
+            n2.Number = 7;
+            n2.AddDetail("B");
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+
+            p1 = new Point();
+        }
+    }
+}
