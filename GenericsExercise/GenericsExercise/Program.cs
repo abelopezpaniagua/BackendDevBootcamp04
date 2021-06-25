d501bc9efa51112a3ef14987411ac34016d70a89
Abel Lopez <abelopezpaniagua@gmail.com>
2021-06-25 16:04:25 -0400

    Generic Exercise

---
+++ GenericsExercise/GenericsExercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GenericsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            char c, d;
+            a = 10;
+            b = 20;
+            c = 'I';
+            d = 'V';
+
+            Console.WriteLine("Before calling swap: ");
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+            Console.WriteLine("c = {0}, d = {1}", c, d);
+
+            Swap<int>(ref a, ref b);
+            Swap<char>(ref c, ref d);
+
+            Console.WriteLine("After calling swap: ");
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+            Console.WriteLine("c = {0}, d = {1}", c, d);
+        }
+
+        private static void Swap<T>(ref T a, ref T b)
+        {
+            T aux = a;
+            a = b;
+            b = aux;
+        }
+    }
+}
