8d80ef0595553e52bfdb2a298a8f44062009a79c
Abel Lopez <abelopezpaniagua@gmail.com>
2021-06-25 16:32:59 -0400

    Generic constraints

---
+++ GenericConstraints/GenericConstraints/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+namespace GenericConstraints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyGeneric<ArrayList> x = new();
+        }
+
+        public class MyGeneric<T> where T : ICollection
+        {
+
+        }
+
+        public class MyGenericClass
+        {
+            public MyGenericClass()
+            {
+
+            }
+        }
+
+        public class AnotherClass : MyGenericClass
+        {
+
+        }
+
+        public class AnotherClass2
+        {
+
+        }
+    }
+}
