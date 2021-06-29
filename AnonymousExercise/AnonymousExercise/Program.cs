27b5060cf8f83eacb7618d5312603aeaa6a150c2
Abel Lopez <abelopezpaniagua@gmail.com>
2021-06-29 15:55:16 -0400

    AnonymousExercise

---
+++ AnonymousExercise/AnonymousExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AnonymousExercise
+{
+    class Program
+    {
+        class Anonymous
+        {
+            public object getData()
+            {
+                return new { Name = "Pepe", EmailID = "pepe@gmail.com" };
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Anonymous anonymous = new Anonymous();
+            dynamic anonymousDynamicData = anonymous.getData();
+            Console.WriteLine(string.Format("{0} {1}", anonymousDynamicData.Name, anonymousDynamicData.EmailID));
+
+            object anonymousData = anonymous.getData();
+            var obj = Cast(anonymousData, new { Name = "", EmailID = "" });
+            Console.WriteLine(string.Format("{0} {1}", obj.Name, obj.EmailID));
+        }
+
+        static T Cast<T>(object obj, T type) { return (T)obj; }
+    }
+}
