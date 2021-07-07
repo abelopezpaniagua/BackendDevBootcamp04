8d1f345b692350f4bb589bd99d7b8aa8e7f3ad4c
Abel Lopez <abelopezpaniagua@gmail.com>
2021-07-06 21:07:26 -0400

    upload terminal finish

--- DelegatesExercise/DelegatesExercise/Program.cs
+++ DelegatesExercise/DelegatesExercise/Program.cs
@@ -7,7 +7,7 @@ namespace DelegatesExercise
         static void Main(string[] args)
         {
             Expression expression = new(20, 10);
-            var result = expression.ApplyOperator(Operation.Substract);
+            var result = expression.ApplyOperator(Operation.Sum);
             Console.WriteLine($"Result: {result}");
         }
     }
