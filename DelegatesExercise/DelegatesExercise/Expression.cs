fdd5768a7d1cb61fab34e56fc78e0c20293fceae
Abel Lopez <abelopezpaniagua@gmail.com>
2021-07-06 21:05:32 -0400

    Delegates exercise

---
+++ DelegatesExercise/DelegatesExercise/Expression.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesExercise
+{
+    public enum Operation { Sum, Substract, Multiply }
+
+    public class Expression
+    {
+        private int first;
+        private int second;
+
+        private Dictionary<Operation, Func<int>> operations = new();
+
+        public Expression(int first, int second)
+        {
+            this.first = first;
+            this.second = second;
+
+            this.operations.Add(Operation.Sum, Sum);
+            this.operations.Add(Operation.Substract, Substract);
+            this.operations.Add(Operation.Multiply, Multiply);
+        }
+
+        private int Sum()
+        {
+            return first + second;
+        }
+
+        private int Substract()
+        {
+            return first - second;
+        }
+
+        private int Multiply()
+        {
+            return first * second;
+        }
+
+        public int ApplyOperator(Operation operation)
+        {
+            if (this.operations.ContainsKey(operation))
+            {
+                return this.operations[operation]();
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ DelegatesExercise/DelegatesExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DelegatesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expression expression = new(20, 10);
+            var result = expression.ApplyOperator(Operation.Substract);
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
