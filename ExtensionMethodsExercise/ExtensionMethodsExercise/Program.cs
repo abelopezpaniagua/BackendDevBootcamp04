253d65d598bc9630de6727eb3f28d9f22f72d98a
Abel Lopez <abelopezpaniagua@gmail.com>
2021-07-13 16:38:46 -0400

    extension methods exercise

---
+++ ExtensionMethodsExercise/ExtensionMethodsExercise/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ExtensionMethodsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomLogger logger = new();
+
+            logger.ShowLog("Default log message", "Default");
+
+            logger.ErrorLog("Something wrong happened!");
+            logger.WarningLog("Notice message");
+            logger.SuccessLog("Success message!");
+        }
+    }
+
+    public class CustomLogger
+    {
+        public void ShowLog(string message, string typeMessage)
+        {
+            Console.WriteLine($"{typeMessage}: {message}");
+        }
+    }
+
+
+    public static class ExtensionLoggerMethods
+    {
+        public static void ErrorLog(this CustomLogger logger, string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            logger.ShowLog(message, "Error");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void WarningLog(this CustomLogger logger, string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            logger.ShowLog(message, "Warning");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void SuccessLog(this CustomLogger logger, string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            logger.ShowLog(message, "Success");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
