75ab37d70afb8eb16d96a4b63a4304f2e7886477
Abel Lopez <abelopezpaniagua@gmail.com>
2021-07-15 14:22:05 -0400

    ExtensionMethods exercise for most common and less common used

---
+++ LINQExtensionMethodsExercise/LINQExtensionMethodsExercise/MyLinqExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Linq
+{
+    public static class MyLinqExtensions
+    {
+        public static IEnumerable<T> ProcessSequence<T>(this IEnumerable<T> sequence)
+        {
+            return sequence;
+        }
+
+        public static int? Median(this IEnumerable<int?> sequence)
+        {
+            var ordered = sequence.OrderBy(item => item);
+            int middlePosition = ordered.Count() / 2;
+
+            return ordered.ElementAt(middlePosition);
+        }
+
+        public static int? Median<T>(this IEnumerable<T> sequence, Func<T, int?> selector)
+        {
+            return sequence.Select(selector).Median();
+        }
+
+        public static int? Mode(this IEnumerable<int?> sequence)
+        {
+            var mostCommon = sequence
+                .GroupBy(item => item)
+                .OrderByDescending(item => item.Count())
+                .First()
+                .Key;
+
+            return mostCommon;
+        }
+
+        public static int? Mode<T>(this IEnumerable<T> sequence, Func<T, int?> selector)
+        {
+            return sequence.Select(selector).Mode();
+        }
+
+        public static int? LessCommon(this IEnumerable<int?> sequence)
+        {
+            var lessCommon = sequence
+                .GroupBy(item => item)
+                .OrderBy(item => item.Count())
+                .First()
+                .Key;
+
+            return lessCommon;
+        }
+
+        public static int? LessCommon<T>(this IEnumerable<T> sequence, Func<T, int?> selector)
+        {
+            return sequence.Select(selector).LessCommon();
+        }
+    }
+}
---
+++ LINQExtensionMethodsExercise/LINQExtensionMethodsExercise/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQExtensionMethodsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Extension MEthods
+
+            List<Product> products = new List<Product>()
+            {
+                new Product() { Id = 1, Price = 35 },
+                new Product() { Id = 2, Price = 150 },
+                new Product() { Id = 3, Price = 650 },
+                new Product() { Id = 4, Price = 150 },
+                new Product() { Id = 5, Price = 15 },
+                new Product() { Id = 6, Price = 35 },
+                new Product() { Id = 7, Price = 650 },
+                new Product() { Id = 8, Price = 78 },
+                new Product() { Id = 9, Price = 35 },
+                new Product() { Id = 10, Price = 78 },
+            };
+
+            Console.WriteLine($"Average: {products.Average(p => p.Price)}");
+            Console.WriteLine($"Median: {products.Median(p => p.Price)}");
+            Console.WriteLine($"Mode: {products.Mode(p => p.Price)}");
+            Console.WriteLine($"Less Common: {products.LessCommon(p => p.Price)}");
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+    }
+}
