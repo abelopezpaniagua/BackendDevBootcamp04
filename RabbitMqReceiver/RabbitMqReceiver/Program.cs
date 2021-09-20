27c9c1c3b84a2ceb332ca5e33d2293d8fb451e62
Abel Lopez Paniagua <abel.lopez@jala-foundation.org>
2021-09-20 16:46:49 -0400

    event driven architecture pattern

---
+++ RabbitMqReceiver/RabbitMqReceiver/Program.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMqReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Listening Sender Messages...:");
+
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = ConnectionFactory.DefaultUser,
+                Password = ConnectionFactory.DefaultPass
+            };
+
+            const string CONFIG_QUEUE = "configQueue";
+
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: CONFIG_QUEUE,
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, args) =>
+                {
+                    var body = args.Body;
+                    var message = Encoding.UTF8.GetString(body.ToArray());
+
+                    Console.WriteLine($"Message from Sender: {message}");
+
+                    channel.BasicAck(deliveryTag: args.DeliveryTag, multiple: false);
+
+                    Thread.Sleep(1000);
+                };
+
+                channel.BasicConsume(queue: CONFIG_QUEUE, autoAck: false, consumer: consumer);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ RabbitMqSender/RabbitMqSender/Program.cs
@@ -0,0 +1,66 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMqSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = "";
+            var continueMessaging = false;
+
+            do
+            {
+                Console.WriteLine("Send a message to the consumer:");
+                message = Console.ReadLine();
+                SendMessage(message);
+
+                Console.WriteLine("Continue messaging?: y/n");
+                continueMessaging = Console.ReadLine().Substring(0, 1) == "y" ? true : false;
+                
+            } while (continueMessaging);
+        }
+
+        static void SendMessage(string messageText)
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = ConnectionFactory.DefaultUser,
+                Password = ConnectionFactory.DefaultPass
+            };
+            const string CONFIG_QUEUE = "configQueue";
+
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: CONFIG_QUEUE,
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+
+                int countTimes = 1;
+
+                while (countTimes <= 20)
+                {
+                    string message = $"Message #{countTimes} - {messageText}";
+                    var body = Encoding.UTF8.GetBytes(message);
+                    
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: CONFIG_QUEUE,
+                        basicProperties: null,
+                        body: body);
+
+                    countTimes++;
+                }
+
+            }
+        }
+    }
+}
